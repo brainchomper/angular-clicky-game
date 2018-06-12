9f712726f1ec816595faa1d2ceca26b58b54eee9
Kevin Smith <kevin@kvnsmith.com>
2018-06-11 20:59:45 -0600

    working initail files

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = props => (
+	<nav className="navbar fixed-top navbar-dark navbar-custom p-3">
+		<span className="navbar-brand">Clicky Game</span>
+		<span className="navbar-text text-white" href="http://kvnsmith.com/portfolio">Portfolio</span>
+		<span className="navbar-text text-white bold">{props.message}</span>
+		<span className="navbar-text text-white">Score: {props.score}, Top Score: {props.topScore}</span>
+	</nav>
+);
+
+export default Navbar;
